3029cd1d8bf672fc2dd039bbc9ede8828ebf0f0a
pandajiny <astic1764@gmail.com>
2021-01-28 15:57:40 +0900

    update repo

--- backend/src/db/query/group.query.ts
+++
@@ -1,9 +0,0 @@
-import { DB_TABLES } from '.';
-
-export function getDeleteGroupQuery(args: { groupId: string }): string {
-  const { groupId } = args;
-  return `
-    DELETE FROM ${DB_TABLES.TODO_GROUP_TABLE}
-    WHERE group_id = "${groupId}"
-  `;
-}
